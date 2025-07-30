1f8372d1512595511f268cbaaf94b4bda4037e2b
Jackson dos Santos <jackson1santos@proton.me>
2025-07-30 13:25:45 -0300

    feat: adiciona pagina e componente NotFound

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute } from '@tanstack/react-router'
 import { Layout } from '@pages/Layout'
+import PageNotFound from '@pages/NotFound'
 
 
 export const Route = createRootRoute({
   component: () => <Layout />,
+  notFoundComponent: () => <PageNotFound />
 })
 
 
